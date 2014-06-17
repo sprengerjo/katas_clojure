9ed2e3260135a157b0588d663c6b7b82cd619df5
unknown <j.sprenger@NB-SPRENGER.gbtecdb.local>
2014-06-17 16:42:02 +0200

    finished refactoring: use streams instead of recursion

--- src/sprengerjo/bowlingGame.clj
+++ src/sprengerjo/bowlingGame.clj
@@ -1,6 +1,20 @@
 (ns sprengerjo.bowlingGame)
  
+(defn remove-last-strike [pins]
+  (if (= 10 (last pins)) 
+    (drop-last pins) 
+       pins))
+
+(defn frame-bonus [a b c]
+  (cond 
+    (= 10 a) [b c] 
+    (= 10 (+ a b)) [c] 
+    :else []))
+
+(defn bonus [pins]
+  (mapcat frame-bonus pins 
+     (drop 1 pins) 
+       (drop 2 (remove-last-strike pins))))
+
 (defn score [pins]
- (reduce + (concat pins (mapcat 
-        (fn [a b c] (cond (= 10 a) [b c] (= 10 (+ a b)) [c] :else []))
-             pins (drop 1 pins) (drop 2 pins)))))
+  (reduce + (concat pins (bonus pins))))
