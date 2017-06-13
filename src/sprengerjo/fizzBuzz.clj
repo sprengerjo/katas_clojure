ca72dc94050848c2bedca6865dd0272e1d5ca487
sprengerjo <sprengerj22@gmail.com>
2017-06-13 21:48:33 +0200

    fizzBuzz: replace lookup table with string concat

--- src/sprengerjo/fizzBuzz.clj
+++ src/sprengerjo/fizzBuzz.clj
@@ -1,11 +1,15 @@
 (ns sprengerjo.fizzBuzz)
 
-(defn fizz-buzz [i]
-  (-> i
-      fizz-buzz-upTo
-      (nth i)
-      ))
+(defn fb [f b i]
+  (str f b (when (clojure.string/blank? (str f b)) i)))
 
 (defn fizz-buzz-upTo [n]
-  (let [c constantly s #(str (inc %)) [f b] [(c "fizz") (c "buzz")] fb (c (str (f) (b)))]
-    (take n (map-indexed #(%2 %) (cycle [s s f s b f s s f b s f s s fb])))))
+  (take n (map fb
+               (cycle ["" "" "fizz"])
+               (cycle ["" "" "" "" "buzz"])
+               (iterate inc 1))))
+
+(defn fizz-buzz [i]
+  (nth (fizz-buzz-upTo i) (dec i)))
+
+
